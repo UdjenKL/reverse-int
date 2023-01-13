440a58c3d6227eeff1e241a2f6a5ee0bd0e359f9
Yauhen Klimovich <webrubin1987@gmail.com>
2023-01-13 13:57:38 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+
+    let result = Math.abs(n); // абсолютное значение
+
+    if (n) {
+       return result.toString().split('').reverse().join('');
+    } else {
+        console.log('Hello RSS!');
+    }
 }
